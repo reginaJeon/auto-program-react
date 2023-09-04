7f5fac67ef7ff966cfa67db353d0e4436605c033
reginaJeon <reginaJeon>
2023-09-04 21:37:52 +0900

    init

---
+++ src/data/index.js
@@ -0,0 +1,6 @@
+export * from "@/data/features-data";
+export * from "@/data/team-data";
+export * from "@/data/contact-data";
+export * from "@/data/projects-data";
+export * from "@/data/authors-table-data";
+export * from "@/data/projects-table-data";
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export * from "@/pages/home";
+export * from "@/pages/profile";
+export * from "@/pages/programs";
+export * from "@/pages/tables";
+export * from "@/pages/sign-in";
+export * from "@/pages/sign-up";
